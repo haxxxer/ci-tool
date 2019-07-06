3a3236e331c2d655afb7b4bae289048fe2a4ceb0
haxxxer <q66p@hotmail.com>
2019-07-06 12:35:31 +0300

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-wget https://srv-file1.gofile.io/download/LkidZM/readme
-wget https://srv-file1.gofile.io/download/LkidZM/ci.sh
+wget https://github.com/haxxxer/ci-tool/blob/master/readme
+wget https://github.com/haxxxer/ci-tool/blob/master/ci.sh
 apt-get install xclip
 chmod 774 ci.sh
 cp ci.sh /usr/local/bin/ci
