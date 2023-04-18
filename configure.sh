157eb68f7ea4061e2e26e5e86949316457313683
haxxxer <q66p@hotmail.com>
2023-04-18 21:50:45 +0300

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-wget https://github.com/haxxxer/ci-tool/blob/master/readme
-wget https://github.com/haxxxer/ci-tool/blob/master/ci.sh
-apt-get install xclip
-chmod 774 ci.sh
-cp ci.sh /usr/local/bin/ci
-rm ci.sh
+wget https://raw.githubusercontent.com/haxxxer/ci-tool/master/ci.sh
+
+apt-get -y install xclip
+
+chmod 755 ci.sh
+
+mv ci.sh /usr/local/bin/ci
+
